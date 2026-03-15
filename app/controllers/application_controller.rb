98e5bc5592e794e57b9cbb88a8bcd12540b1b06c
Clara Lima <mclaralimap@gmail.com>
2026-03-14 21:20:01 -0300

    style: update auth and jobs page design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_jobs
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    jobs_path
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
